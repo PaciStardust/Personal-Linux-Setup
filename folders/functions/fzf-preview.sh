7832064dca065d473f91189d970e75a68f51a35b
Paci <89483951+PaciStardust@users.noreply.github.com>
2025-09-18 11:30:01 +0200

    Preview fix?

--- folders/functions/fzf-preview.sh
+++ folders/functions/fzf-preview.sh
@@ -2,7 +2,11 @@
 if [ -f "$1" ]; then
     stat "$1" --printf='Type: %F | Size: %s | Access: %A\nGroup: %G(%g) | Owner: %U(%u)\nCreated: %w\nModified: %y\n'
     echo '--------------------------------------------------------------------------------------------------------'
-    bat "$1" --style=plain --color=always --theme="$BAT_THEME" --line-range=:500
+    if command -v batcat >/dev/null; then
+        batcat "$1" --style=plain --color=always --theme="$BAT_THEME" --line-range=:500
+    elif 
+        bat "$1" --style=plain --color=always --theme="$BAT_THEME" --line-range=:500
+    fi
 elif [ -d "$1" ]; then
     stat "$1" --printf='Type: %F | Size: %s | Access: %A\nGroup: %G(%g) | Owner: %U(%u)\nCreated: %w\nModified: %y\n'
     echo '--------------------------------------------------------------------------------------------------------'
